7f3bc728f0e1cd59290c70720677b9d1822a9d91
Fernando Zhapa-Camacho <ferzcam@gmail.com>
2023-03-19 11:05:12 +0300

    added baseline models

---
+++ src/models/baseline.py
@@ -0,0 +1,77 @@
+from src.models.base_model import Model, subsumption_rel_name
+
+import torch.optim as optim
+import torch as th
+from tqdm import trange, tqdm
+import logging
+
+
+class Baseline(Model):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def train(self):
+        optimizer = optim.Adam(self.model.parameters(), lr=0.000001, weight_decay = self.weight_decay)
+        min_lr = self.lr/100 
+        max_lr = self.lr
+        scheduler = th.optim.lr_scheduler.CyclicLR(optimizer, base_lr=min_lr,
+                                                   max_lr=max_lr, step_size_up = 30,
+                                                   cycle_momentum = False)
+                        
+        self.model = self.model.to(self.device)
+
+        graph_dataloader = self.create_graph_train_dataloader()
+
+        tolerance = 0
+        best_loss = float("inf")
+        ont_classes_idxs = th.tensor(list(self.ontology_classes_idxs), dtype=th.long,
+                                     device=self.device)
+
+        for epoch in trange(self.epochs, desc=f"Training..."):
+            logging.info(f"Epoch: {epoch+1}")
+            self.model.train()
+
+            graph_loss = 0
+            for head, rel, tail in tqdm(graph_dataloader, desc="Processing batches"):
+                head = head.to(self.device)
+                rel = rel.to(self.device)
+                tail = tail.to(self.device)
+                
+                data = (head, rel, tail)
+                pos_logits = self.model.forward(data)
+
+                neg_logits = 0
+                for i in range(self.num_negs):
+                    neg_tail = th.randint(0, len(self.node_to_id), (len(head),), device=self.device)
+                    data = (head, rel, neg_tail)
+                    neg_logits += self.model.forward(data)
+                neg_logits /= self.num_negs
+                
+                batch_loss= th.relu(pos_logits - neg_logits + self.margin).mean()
+
+                optimizer.zero_grad()
+                batch_loss.backward()
+                optimizer.step()
+
+                if scheduler is not None:
+                    scheduler.step()
+
+                graph_loss += batch_loss.item()
+
+            graph_loss /= len(graph_dataloader)
+
+            if best_loss > graph_loss:
+                best_loss = graph_loss
+                th.save(self.model.state_dict(), self.model_path)
+                tolerance = self.initial_tolerance
+                print("Model saved")
+            else:
+                tolerance -= 1
+                if tolerance == 0:
+                    print("Early stopping")
+                    break
+
+            print(f"Train loss: {graph_loss:.6f}\n")
+
+
+    
